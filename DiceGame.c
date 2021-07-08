f8b8b84d425e319aff9d3a618331b481f233ac1e
s1280197 <s1280197@call2dc24.u-aizu.ac.jp>
2021-07-08 12:51:38 +0900

    Initial revision

---
+++ DiceGame.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+  int i,j;
+
+  printf("Rolling the dice..\n");
+ 
+  i = rand() %6 + 1;
+  j = rand() %6 + 1;
+
+  printf("Die 1: %d\n",i);
+  printf("Die 2: %d\n",j);
+
+  printf("Total value: %d\n",i+j);
+
+
+  return 0;
+}
